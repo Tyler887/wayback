5bce7a7272db679a33855568475d4b819020567c
Tyler887 <tylermageeshields@gmail.com>
2022-02-07 19:28:12 +0000

    Update WB_test.py

--- WB_test.py
+++ WB_test.py
@@ -8,11 +8,11 @@ import requests
 
 # HTTP is used to call the Internet Archive Wayback Machine servers, NOT the API!
 
-try:
 
-  r = requests.head("https://web.archive.org/save/" + archive) # This saves to the Wayback Machine.
 
-  if not r.status_code.startswith("20") and not r.statuscode.startswith("30"): # 20X is OK, 30X is redirect. Other codes are ERRORs.
+r = requests.head("https://web.archive.org/save/" + archive) # This saves to the Wayback Machine.
+
+if not r.status_code.startswith("20") and not r.statuscode.startswith("30"): # 20X is OK, 30X is redirect. Other codes are ERRORs.
 
     print("Archived to Wayback Machine UNSUCCESSFULLY. web.archive.org returned HTTP code " + r.status_code + ".")
 
@@ -20,13 +20,9 @@ try:
 
     exit(1)
 
-expect requests.ConnectionError:
-
-  print("Archived to Wayback Machine UNSUCCESSFULLY. Could not establish a working connection.\nAre you sure that there are no current incidents reported by the the Internet Archive?")
 
 
-
-  exit(1)
+  
 
 print("Archived to Wayback Machine successfully.")
 
